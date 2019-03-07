ac54da411e01fff90e18562215eb7d06dbf11d22
handsomX <javanav@163.com>
2019-03-07 13:43:27 +0800

    image tool

--- xtools/src/main/java/constxiong/xtools/picture/ImageUtil.java
+++ xtools/src/main/java/constxiong/xtools/picture/ImageUtil.java
@@ -3,12 +3,23 @@ package constxiong.xtools.picture;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.util.Iterator;
 
 import javax.imageio.ImageIO;
+import javax.imageio.ImageReadParam;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * 图片工具
@@ -16,6 +27,8 @@ import javax.imageio.ImageIO;
  * @date 2019-03-01 18:00:17
  */
 public class ImageUtil {
+	
+	private static final String FILE_CONNECTOR_POINT = "\\.";
 	
 	/**
 	 * 绘制图片
@@ -55,33 +68,163 @@ public class ImageUtil {
 	 * @param y 裁剪开始y坐标
 	 * @param width 裁剪宽度
 	 * @param height 裁剪高度
+	 * @param formatName 图片类型
 	 */
-	public static boolean cutImage(String srcPath, String destPath, int x, int y, int width, int height) {
+	public static boolean cutImage(String srcPath, String destPath, int x, int y, int width, int height, String formatName) {
+		boolean cutSuccess = false;
+		Iterator<ImageReader> ite = ImageIO.getImageReadersByFormatName(formatName);
+		if (ite.hasNext()) {
+			ImageReader reader = ite.next();
+			InputStream is = null;
+			try {
+				is = new FileInputStream(srcPath);
+				ImageInputStream iis = ImageIO.createImageInputStream(is);
+				reader.setInput(iis);
+				ImageReadParam defaultReadParam = reader.getDefaultReadParam();
+				Rectangle rec = new Rectangle(x, y, width, height);
+				defaultReadParam.setSourceRegion(rec);
+				BufferedImage bi = reader.read(0, defaultReadParam);
+				cutSuccess =  ImageIO.write(bi, formatName, new File(destPath));
+			} catch (IOException e) {
+				System.out.println("cut image["+srcPath+"] failed");
+				e.printStackTrace();
+			} finally {
+				if (is != null) {
+					try {
+						is.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		} else {
+			throw new RuntimeException("不支持的格式["+ formatName +"]");
+		}
+		return cutSuccess;
+	}
+	
+	/**
+	 * 判断文件后缀是否为图片文件格式,bmp|gif|jpg|jpeg|png 返回true
+	 * @param imageFileSuffix 图片文件后缀名
+	 * @return
+	 */
+	public static boolean isImageBySuffix(String imageFileSuffix) {
+		if (StringUtils.isNotEmpty(imageFileSuffix)) {
+			//[JPG, jpg, bmp, BMP, gif, GIF, WBMP, png, PNG, wbmp, jpeg, JPEG]
+			String[] formatNames = ImageIO.getReaderFormatNames();
+			if (ArrayUtils.isNotEmpty(formatNames)) {
+				for (String formatName : formatNames) {
+					if (imageFileSuffix.toLowerCase().equals(formatName)) {
+						return true;
+					}
+				}
+			}
+		}
 		return false;
 	}
 	
 	/**
-	 * 判断文件是否为
-	 * @param srcPath
+	 * 判断文件是否为图片文件格式, bmp|gif|jpg|jpeg|png 后缀的图片文件返回true
+	 * @param imageName 图片文件名
 	 * @return
 	 */
-	public static boolean isImage(String srcPath) {
+	public static boolean isImageByFileName(String imageName) {
+		if (StringUtils.isNotEmpty(imageName)) {
+			String[] imageNames = imageName.split(FILE_CONNECTOR_POINT);
+			return isImageBySuffix(imageNames[imageNames.length - 1]);
+		}
 		return false;
 	}
 	
+	/**
+	 * 判断文件是否为合法图片
+	 * @param srcPath 图片文件绝对路径
+	 * @param checkImageName 是否校验图片文件名
+	 * @return
+	 */
+	public static boolean isImage(String srcPath, boolean checkImageName) {
+		if (checkImageName) {
+			return isImageByFileName(srcPath);
+		} else {
+			if (StringUtils.isNotEmpty(srcPath)) {
+				File imageFile = new File(srcPath);
+				if (imageFile.isFile() && imageFile.length() > 0) {
+					return isImage(imageFile);
+				}
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * 判断文件流是否为合法图片
+	 * @param file 图片文件
+	 * @return
+	 */
+	public static boolean isImage(File file) {
+		return isImage((Object)file);
+	}
+	
+	/**
+	 * 判断文件流是否为合法图片
+	 * @param srcInputStream 图片文件的流
+	 * @return
+	 */
 	public static boolean isImage(InputStream srcInputStream) {
+		return isImage((Object)srcInputStream);
+	}
+	
+	/**
+	 * 判断文件流是否为合法图片
+	 * @param srcInputStream 图片文件的流
+	 * @return
+	 */
+	public static boolean isImage(URL url) {
+		return isImage((Object)url);
+	}
+	
+	/**
+	 * 图片文件读取
+	 * @param obj (URL|InputStream|File)
+	 * @return
+	 */
+	private static boolean isImage(Object obj) {
+		Image image = null;
+		if (obj != null) {
+			try {
+				if (obj instanceof URL) {
+					image = ImageIO.read((URL)obj);
+				} else if (obj instanceof InputStream) {
+					image = ImageIO.read((InputStream)obj);
+				} else if (obj instanceof File) {
+					image = ImageIO.read((File)obj);
+				} else {
+					throw new IllegalArgumentException("不支持这种类型["+obj.getClass().getCanonicalName()+"]");
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			if (image != null && image.getWidth(null) > 0 && image.getHeight(null) > 0) {
+				return true;
+			}
+		}
 		return false;
 	}
 	
-	public static boolean compress (String srcPath, String destPath, float quality) {
+	public static boolean compressImage (String srcPath, String destPath, float quality) {
 		return false;
 	}
 	
-	public static boolean resize (String srcPath, String destPath, int newWith, int newHeight) {
+	public static boolean resizeImage (String srcPath, String destPath, int newWith, int newHeight) {
+		return false;
+	}
+	
+	public static boolean zoomImage (String srcPath, String destPath, int newWith, int newHeight) {
 		return false;
 	}
 	
 	public static boolean downloadImage() {
 		return false;
 	}
+	
 }
--- xtools/src/test/java/constxiong/xtools/picture/ImageUtilTest.java
+++ xtools/src/test/java/constxiong/xtools/picture/ImageUtilTest.java
@@ -14,16 +14,38 @@ import constxiong.xtools.path.PathUtil;
  * @date 2019-03-04 16:28:11
  */
 public class ImageUtilTest {
+	
+	private static final String IMAGE_PATH = PathUtil.getProjectPath() + File.separator + "images" + File.separator;
 
+	/**
+	 * 测试图片绘制
+	 * @throws IOException
+	 */
 	@Test
 	public void testDrawImage() throws IOException {
 		BufferedImage bufferedImage = new BufferedImage(200, 200, BufferedImage.TYPE_INT_BGR);
-		String imagePath = PathUtil.getProjectPath() + File.separator + "images" + File.separator;
-		File dir = new File(imagePath); 
+		File dir = new File(IMAGE_PATH); 
 		if (!dir.exists()) {
 			dir.mkdirs();
 		}
 		ImageUtil.drawSimpleImage(bufferedImage, "jpg", 
-				new File(imagePath + "simple.jpg"));
+				new File(IMAGE_PATH + "simple.jpg"));
+	}
+	
+	/**
+	 * 测试图片剪切
+	 */
+	@Test
+	public void testCutImage() {
+		String imageName = "simple.jpg";
+		String cutImageName = "simple_cut.jpg";
+		String srcPath = IMAGE_PATH + imageName;
+		String destPath  = IMAGE_PATH + cutImageName;
+		int x = 10; 
+		int y = 40;
+		int width = 180;
+		int height = 120;
+		String formatName = "jpg";
+		ImageUtil.cutImage(srcPath, destPath, x, y, width, height, formatName);
 	}
 }
