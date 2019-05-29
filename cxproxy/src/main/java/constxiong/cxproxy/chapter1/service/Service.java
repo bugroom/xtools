82260c5618abcb35dd442fe8f4ed72099664eac8
handsomX <javanav@163.com>
2019-05-29 11:25:14 +0800

    cxproxy chapter 1

---
+++ cxproxy/src/main/java/constxiong/cxproxy/chapter1/service/Service.java
@@ -0,0 +1,16 @@
+package constxiong.cxproxy.chapter1.service;
+
+import java.util.Map;
+
+/**
+ * 服务接口
+ * @author ConstXiong
+ * @date 2019-05-29 11:02:02
+ */
+public interface Service {
+
+	boolean login(String username, String password);
+
+	Map<String, Object> getUserInfo(String username);
+
+}
---
+++ cxproxy/src/main/java/constxiong/cxproxy/chapter1/service/ServiceImpl.java
@@ -0,0 +1,43 @@
+package constxiong.cxproxy.chapter1.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 服务接口实现
+ * @author ConstXiong
+ * @date 2019-05-29 11:02:15
+ */
+public class ServiceImpl implements Service {
+
+	@Override
+	public boolean login(String username, String password) {
+		simulateDaOperation(100);
+		System.out.println("用户名：" + username + ", 密码：" + password + "  登录成功");
+		return true;
+	}
+
+	@Override
+	public Map<String, Object> getUserInfo(String username) {
+		Map<String, Object> userInfo = new HashMap<String, Object>();
+		simulateDaOperation(100);
+		userInfo.put("username", username);
+		userInfo.put("sex", "男");
+		userInfo.put("age", 18);
+		System.out.println("用户名：" + username + ", 获取用户信息：" + userInfo);
+		return userInfo;
+	}
+	
+	/**
+	 * 模拟数据库操作，休眠
+	 * @param millis 毫秒数
+	 */
+	private void simulateDaOperation(long millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
