defb5cd82488dde830b4a90fe25c7bba65964778
handsomX <javanav@163.com>
2019-07-16 11:13:50 +0800

    spring inject

---
+++ interview/src/main/java/constxiong/interview/inject/Bowl.java
@@ -0,0 +1,18 @@
+package constxiong.interview.inject;
+
+//import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Repository;
+//import org.springframework.stereotype.Service;
+
+//@Component //注册所有bean
+//@Controller //注册控制层的bean
+//@Service //注册服务层的bean
+//@Repository //注册dao层的bean
+public class Bowl {
+
+	public void putRice() {
+		System.out.println("盛饭...");
+	}
+
+}
---
+++ interview/src/main/java/constxiong/interview/inject/BowlFactory.java
@@ -0,0 +1,13 @@
+package constxiong.interview.inject;
+
+public class BowlFactory {
+
+	public static final Bowl getBowl() {
+		return new Bowl();
+	}
+	
+//	public Bowl getBowl() {
+//		return new Bowl();
+//	}
+	
+}
