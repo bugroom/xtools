681949107c12e661acf8701bd56c83876c4c6608
handsomX <javanav@163.com>
2019-07-26 14:42:07 +0800

    test throws

---
+++ interview/src/main/java/constxiong/interview/TestThrows.java
@@ -0,0 +1,12 @@
+package constxiong.interview;
+
+import java.io.IOError;
+
+public class TestThrows {
+
+	public void test() {
+		throw new IOError(null);
+//		throw new RuntimeException();
+	}
+	
+}
