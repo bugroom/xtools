8cc2a216dbf808437255e0f0c00bd5af800b4fb7
handsomX <javanav@163.com>
2019-11-21 19:01:07 +0800

    'interview'

--- interview/src/main/java/constxiong/interview/TestInterfaceMultipleExtend.java
+++ interview/src/main/java/constxiong/interview/TestInterfaceMultipleExtend.java
@@ -1,14 +1,14 @@
-package constxiong.interview;
-
-import java.util.List;
-import java.util.Set;
-
-/**
- * 接口多继承
- * @author ConstXiong
- * @date 2019-04-18 14:48:06
- * @param <E>
- */
-public interface TestInterfaceMultipleExtend<E> extends Set<E>, List<E>{
-
-}
+//package constxiong.interview;
+//
+//import java.util.List;
+//import java.util.Set;
+//
+///**
+// * 接口多继承(JDK1.5可以，JDK1.8不可以)
+// * @author ConstXiong
+// * @date 2019-04-18 14:48:06
+// * @param <E>
+// */
+//public interface TestInterfaceMultipleExtend<E> extends Set<E>, List<E>{
+//
+//}
